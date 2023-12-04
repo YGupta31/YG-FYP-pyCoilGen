d1475eb56db42a4b00eb4bd4414688612f31e26c
YGupta31 <gupta.y0131@gmail.com>
2023-12-04 09:25:01 +0000

    update pycoilgen

--- examples/biplanar_xgradient.py
+++ examples/biplanar_xgradient.py
@@ -9,11 +9,9 @@ import logging
 # Local imports
 from pyCoilGen.pyCoilGen_release import pyCoilGen
 from pyCoilGen.sub_functions.constants import DEBUG_BASIC, DEBUG_VERBOSE
-# New code added to eliminate error
 import numpy as np
 import warnings
 np.warnings = warnings
-#######
 if __name__ == '__main__':
     # Set up logging
     log = logging.getLogger(__name__)
@@ -21,42 +19,39 @@ if __name__ == '__main__':
     # logging.basicConfig(level=logging.INFO)
 
     arg_dict = {
-        'field_shape_function': 'z',  # definition of the target field
-        'coil_mesh_file': 'bi_planer_rectangles_width_1000mm_distance_500mm.stl',
-        #'coil_mesh':'create bi-planar mesh',
-        #'biplanar_mesh_parameter_list': [0.35,0.35,5 , 5, 0,0, 1, 0, 0,0,0.2],# 350x600 planar mesh with cuboid elements of 2/mm. Normal to z axis at (0).
-        'target_mesh':'create cylinder mesh',
-        'cylinder_mesh_parameter_list': [0.35, 0.075, 5, 5, 1, 0, 0, 0],
+        'field_shape_function': 'y',  # definition of the target field
+        #'coil_mesh_file': 'bi_planer_rectangles_width_1000mm_distance_500mm.stl',
+        'coil_mesh': 'create planar mesh',
+        'planar_mesh_parameter_list': [0.3, 0.175, 12, 7, 1, 0, 0, 0, -0.15, -0.0875, -0.1],
         'target_mesh_file': 'none',
         'secondary_target_mesh_file': 'none',
-        'secondary_target_weight': 0.5,
-        #'target_region_radius': 0.075,  # in meter image depth of 15 cm => r=0.075
-        'target_region_resolution': 15,  # MATLAB 10 is the default
+        'secondary_target_weight': 0,
+        'target_region_radius': 0.075,  # in meter
+        # 'target_region_resolution': 10,  # MATLAB 10 is the default
         'use_only_target_mesh_verts': False,
-        'target_gradient_strength': 40,
         'sf_source_file': 'none',
         # the number of potential steps that determines the later number of windings (Stream function discretization)
-        'levels': 20,
+        'levels': 14,
         # a potential offset value for the minimal and maximal contour potential ; must be between 0 and 1
         'pot_offset_factor': 0.25,
         'surface_is_cylinder_flag': True,
         # the width for the interconnections are interconnected; in meter
-        'interconnection_cut_width': 0.05,
+        'interconnection_cut_width': 0.5,
         # the length for which overlapping return paths will be shifted along the surface normals; in meter
-        'normal_shift_length': 0.15,
-        'iteration_num_mesh_refinement': 1,  # the number of refinements for the mesh;
-        'set_roi_into_mesh_center': True,
+        'normal_shift_length': 0.005,
+        'iteration_num_mesh_refinement': 2,  # the number of refinements for the mesh;
+        'set_roi_into_mesh_center': False,
         'force_cut_selection': ['high'],
         # Specify one of the three ways the level sets are calculated: "primary","combined", or "independent"
         'level_set_method': 'primary',
         'skip_postprocessing': False,
-        'skip_inductance_calculation': False,
-        'tikhonov_reg_factor': 20,  # Tikhonov regularization factor for the SF optimization
+        'skip_inductance_calculation': True,
+        'tikhonov_reg_factor': 5,  # Tikhonov regularization factor for the SF optimization
 
-        'output_directory': 'trial culinder gradient',  # [Current directory]
-        'project_name': 'biplanar_zgradient_i',
+        'output_directory': 'images',  # [Current directory]
+        'project_name': 'octantiiy',
         'persistence_dir': 'debug',
-        'debug': DEBUG_BASIC,
+        'debug': 0,
     }
 
     result = pyCoilGen(log, arg_dict)
@@ -68,7 +63,7 @@ if __name__ == '__main__':
     from pyCoilGen.helpers.persistence import load
     import pyCoilGen.plotting as pcg_plt
 
-    which = 'biplanar_xgradient_40_20'
+    which = 'octantii'
     solution = load('debug', which, 'final')
     save_dir = f'{solution.input_args.output_directory}'
     makedirs(save_dir, exist_ok=True)
